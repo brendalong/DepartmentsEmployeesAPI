373f0ad587c766978d9beb72b4c2f3fac5746610
Brenda Long <brenda@studio6thave.com>
2020-03-26 09:15:48 -0500

    files for Dept Empl API

---
+++ DepartmentsEmployeesAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DepartmentsEmployeesAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DepartmentsEmployeesAPI.Controllers
+{
+    //Annotations - helpers from framework
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        //constructor
+        public DepartmentController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        //setup the Sql connection
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // ----------------------------
+        //get all departments from DB
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, DeptName FROM Department";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Department> departments = new List<Department>();
+
+                    while (reader.Read())
+                    {
+                        Department department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            DeptName = reader.GetString(reader.GetOrdinal("DeptName"))
+                        };
+                    departments.Add(department);
+                    } 
+
+                reader.Close();
+
+                return Ok(departments);
+                }
+            }
+        }
+        //end get all departments
+
+        //get department by id
+        //this is an override of the previous 'Get' 
+        [HttpGet("{id}", Name = "GetDepartment")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+           using(SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT Id, DeptName
+                    FROM Department
+                    WHERE Id = @id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Department department = null;
+
+                    if (reader.Read())
+                    {
+                        department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            DeptName = reader.GetString(reader.GetOrdinal("DeptName"))
+                        };
+
+                        reader.Close();
+                        return Ok(department);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+        }
+       //end get department by id
+
+        //Does department exists?
+        private bool DepartmentExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT Id, DeptName
+                    FROM Department
+                    WHERE Id = id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
+        //end of Does department exists?
+    }
+}
